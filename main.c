a6b26b5987dfd15b67ad71b3f4cf9a91201f4291
ayeung4p <102668784+ayeung4p@users.noreply.github.com>
2024-09-22 21:41:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+//main.c
 
+#include <stdio.h>
+
+int main() {
+    // This is a comment.
+  
+    /* 
+      This is a multi-lined comment.
+      These gray lines are ignored by the program.
+      It is only for the developer, to remember something,
+      or to tell their peers how the program works.
+    */
+  
+    printf("Hello, World!\n"); // \n means write a newline
+    return 0;
+  
+    // Console:
+    // Hello, World!
+    // 
+}
